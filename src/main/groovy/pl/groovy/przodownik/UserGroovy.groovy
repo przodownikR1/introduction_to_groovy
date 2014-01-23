3001fa91410663ecf4fd29a9a70fbc2f3369fed9
Slawomir Borowiec <przodownik@tlen.pl>
2014-01-23 12:42:52 +0100

    gradle build modify

--- src/main/groovy/pl/groovy/przodownik/UserGroovy.groovy
+++ src/main/groovy/pl/groovy/przodownik/UserGroovy.groovy
@@ -1,6 +1,18 @@
 package pl.groovy.przodownik
 import groovy.transform.Canonical;
 
+/**
+ * @author Sławomir Borowiec 
+ * Module name : introduction_to_groovy
+ * Creating time :  23 sty 2014 10:43:08
+ 
+ */
+/**
+ * @author Sławomir Borowiec 
+ * Module name : introduction_to_groovy
+ * Creating time :  23 sty 2014 10:43:09
+ 
+ */
 @Canonical
 class UserGroovy {
  String firstName
